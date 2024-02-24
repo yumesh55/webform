0bd74836590859a11f09018cd41477a2639b92b5
yumesh55 <56038392+yumesh55@users.noreply.github.com>
2024-02-24 22:38:11 +0530

    24-02-24 Changes

--- web-form/src/App.vue
+++ web-form/src/App.vue
@@ -1,28 +1,49 @@
 <template>
+  <!--Calling regform-->
  <regform/>
 </template>
 
 <script>
+/*Import regform */
 import regform from './components/regform.vue'
 
+
+/*Calling regform*/
 export default {
   name: 'App',
   components: { regform }
 }
 </script>
 
+<!-- Style for entire Page -->
+
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap');
+
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Noto Sans", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 155px;
+  
 }
+
 
 body{
   margin: 0;
   background: #eee;
+  font-family: "Noto Sans", sans-serif;
+  font-style: normal;
+  background-image: url('https://miro.medium.com/v2/resize:fit:1400/1*8mNNZuvAjFWcFCcqEDMUpQ.png');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  min-height: 100vh;
+  overflow-Y: hidden;
+  overflow-X: hidden;
 }
+
 </style>
--- web-form/src/components/regform.vue
+++ web-form/src/components/regform.vue
@@ -1,131 +1,155 @@
 <template>
-<form @submit.prevent="submitForm">
-    <label>Email:</label>
-    <input type="email" required v-model="email" @input="validateForm">
+<!--Form Page Content-->
 
-    <label>Password:</label>
-    <input type="password" required v-model="password" @input="validateForm">
-    <div v-if="passworderror" class="error">{{ passworderror }}</div>
-
-    <label>Select your language</label>
-
-    <select v-model="option">
-        <option value="English">English</option>
-        <option value="Tamil">Tamil</option>
-        <option value="Hindi">Hindi</option>
-    </select>
-
-    <div class="submit">
-        <button :disabled="!isFormValid" type="submit">Get Started</button>
-    </div>
-
-    <div v-if="showAlert" class="alert">
-        Form Submitted Successfully!
+  <form @submit="submitForm">
+    <div class="container">
+    <i class="fa-solid fa-right-to-bracket"></i></div>
+     <i class="fa-regular fa-envelope"></i><label>Email ID:</label>
+      <input type="email" required v-model="email" @input="validateForm">
+  
+      <i class="fa-solid fa-key"></i><label>Password:</label>
+      <input type="password" required  v-model="password" @input="validateForm">
+      <div v-if="showValidationMessage" class="validation-message">Please fill the Password</div>
+  
+      
+      <i class="fa-solid fa-language"></i><label>Select your language</label>
+       <select id="selectOption" v-model="selectedOption" @input="validateForm" required>
+        <option value="">-- Please select --</option>
+          <option value="English">English</option>
+          <option value="Tamil">Tamil</option>
+          <option value="Hindi">Hindi</option>
+      </select>
+  
+      <!--Button-->
+      <div class="submit">
+          <button :disabled="!isFormValid" type="submit">Get Started</button>
       </div>
-
-      
+     </form>
+  </template>
+  <!--Form Content End!!-->
   
-</form>
 
-</template>
-
-<script>
-
-export default {
- data() {
-    return{
-        email : '',
-        password : '',
-        passworderror: '',
-        isFormValid: '',
-        option: '',
-        showAlert: ''
-        
-    };
-  },
-  methods: {
-    validateForm() {
-      if (this.email.trim() !== '' && this.password.trim() !== '') {
-        this.isFormValid = true;
-        
-        
-      } else {
-        this.isFormValid = false;
-      
-  
-        
-      }
-      
+  <!--Script Logic-->
+  <script>
+  export default {
+   data() {
+      return{
+          email : '',
+          password : '',
+          isFormValid: '',
+          selectedOption:''
+      };
     },
-    
-    submitForm() {
+      methods: {
+      validateForm() {
+        if (!this.email.trim() !== '' && this.password.trim() ) {
+          this.isFormValid = true;
+          
+          
+            } else if( !this.password.trim()){
+               this.showValidationMessage = true;
 
-      if (this.isFormValid) {
-
- 
-        console.log('Form submitted successfully');
+            }
+            
+            else {
+          this.isFormValid = false;
+         
+           }
+          },
+  
+      submitForm() {
+              if (this.isFormValid) {
+              alert('Form submitted successfully');
+        }
       }
     }
+  };
+
+   </script>
+  
+   <!--Script Ends-->
+
+ 
+   <!--Sccss style-->
+
+   <style>
+   
+   .container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    }
+  .fa-regular{
+  display: inline-block;
+  vertical-align: middle;
+  margin-right: 5px;
+  line-height: 20px;
+  border-radius: 50%;
+  text-align: center;
   }
-};
-    
+  .fa-solid{
+  display: inline-block;
+  vertical-align: middle;
+  margin-right: 5px;
+  line-height: 20px;
+  border-radius: 50%;
+  text-align: center;
+  }
+  form {
+  max-width: 420px;
+  margin: 30px auto;
+  background: white;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+  font-weight: bold;
+  }
+  label {
+  color: rgb(0, 0, 0);
+  display: inline-block;
+  margin: 25px 0 15px;
+  font-size: 0.6rem;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+  }
+  input, select {
+  display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: solid #ddd;
+  color: #555;
+  }
+  button{
+  background: #0b6dff;
+  border: 0;
+  padding: 10px 20px;
+  margin-top: 20px;
+  color: white;
+  border-radius: 20px;
+  }
+  .submit{
+  text-align: center;
+  }
+  .error{
+  color: #1d570d;
+  margin-top: 10px;
+  font-size: 0.8em;
+  font-weight: bold;
+  }
 
-    
- 
- 
-
-
-</script>
-
-<style>
-form {
-    max-width: 420px;
-    margin: 30px auto;
-    background: white;
-    text-align: left;
-    padding: 40px;
-    border-radius: 10px;
-}
-
-label {
-
-    color: rgb(0, 0, 0);
-    display: inline-block;
-    margin: 25px 0 15px;
-    font-size: 0.6rem;
-    text-transform: uppercase;
-    letter-spacing: 1px;
-    font-weight: bold;
-}
-
-input, select {
-
-    display: block;
-    padding: 10px 6px;
-    width: 100%;
-    box-sizing: border-box;
-    border: none;
-    border-bottom: solid #ddd;
-    color: #555;
-}
-button{
-    background: #0b6dff;
-    border: 0;
-    padding: 10px 20px;
-    margin-top: 20px;
-    color: white;
-    border-radius: 20px;
-}
-
-.submit{
-    text-align: center;
-}
-
-.error{
-
-color: #ff0062;
-margin-top: 10px;
-font-size: 0.8em;
-font-weight: bold;
-}
-</style>
+  .validation-message{
+    color: red;
+  }
+   
+  /*Media query for mobile device*/
+  @media only screen and (max-width: 767px) {
+    .container {
+      padding: 0 100px;
+      background-image: url('C:\Users\YUMKRISH\OneDrive\Desktop\github projects\webform\web-form\src\assets\Figmadesign.png');
+    }
+  }
+  </style>
+  
